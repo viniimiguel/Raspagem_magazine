b3c6831aab314b461a7e09663a670547da6da197
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-08 16:04:45 -0300

    askdp

--- indedx.py
+++ indedx.py
@@ -39,7 +39,7 @@ class Scrapy:
                 "scrap": {
                     "nome": f"/html/body/div[1]/div/main/section[4]/div[3]/div/ul/li[{contador}]/a/div[3]/h2",
                     "preco": f"/html/body/div[1]/div/main/section[4]/div[3]/div/ul/li[{contador}]/a/div[3]/div[2]/div/div/p",
-                    "next":  f"/html/body/div[1]/div/main/section[4]/div[4]/nav/ul/li[9]" #/html/body/div[1]/div/main/section[4]/div[4]/nav/ul/li[9]
+                    "next":  "/html/body/div[1]/div/main/section[4]/div[4]/nav/ul/li[9]"
                 }
             }
 
@@ -52,9 +52,7 @@ class Scrapy:
             # Verifique se o elemento XPath existe, se não, saia do loop
             if not self.elemento_xpath_existe(f"/html/body/div[1]/div/main/section[4]/div[3]/div/ul/li[{contador + 1}]/a/div[3]/h2"):
                 try:
-                    print('entrei no bloco "try"')
                     botao_proximo = self.driver.find_element(By.XPATH,self.site_dados['scrap']['next'])
-                    sleep(3)
                     botao_proximo.click()
                     print('navegando para proxima pagina!!!')
                     contador = 1
